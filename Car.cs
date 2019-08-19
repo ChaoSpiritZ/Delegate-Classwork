8e123e2a12774399dd16c7ff9ce349afd62e2fb1
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2019-08-19 20:19:29 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Classwork
+{
+    class Car
+    {
+        private int _currentSpeed = -1;
+
+        public delegate void Driving(int currentSpeed);
+        public event Driving OnDriving;
+
+        public delegate void Stop();
+        public event Stop OnStop;
+
+        public delegate void Speeding();
+        public event Speeding OnSpeeding;
+
+        public int CurrentSpeed
+        {
+            get
+            {
+                return _currentSpeed;
+            }
+            set
+            {
+                _currentSpeed = value;
+                OnDriving(_currentSpeed);
+                if(_currentSpeed > 120)
+                {
+                    OnSpeeding();
+                }
+                if(_currentSpeed == 0)
+                {
+                    OnStop();
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Car aCar = new Car();
+            aCar.OnSpeeding += new Car.Speeding(p.Speeding);
+            aCar.OnStop += new Car.Stop(p.Stopped);
+            aCar.OnDriving += new Car.Driving(p.Driving);
+
+            aCar.CurrentSpeed = 0;
+            aCar.CurrentSpeed = 100;
+            aCar.CurrentSpeed = 120;
+            aCar.CurrentSpeed = 130;
+            aCar.CurrentSpeed = 140;
+        }
+
+        public void Speeding()
+        {
+            Console.WriteLine("YOU ARE SPEEDING!");
+        }
+
+        public void Stopped()
+        {
+            Console.WriteLine("The car stopped.");
+        }
+
+        public void Driving(int currentSpeed)
+        {
+            Console.WriteLine("You are driving at a speed of: " + currentSpeed);
+        }
+    }
+}
